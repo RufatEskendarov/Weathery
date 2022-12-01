c80cfbbf0f899afb56d660ba4735dcad47f75ba9
Eskendarov <Eskendarov.Rufat@sunbury.ca>
2022-12-01 00:10:41 -0400

    Updated event handler for searched Cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
 "use strict";
 
-var searchList = [];
-var searchSubmitBtn = document.querySelector("#search-submit");
-var cityNameInput = document.querySelector("#city-name");
-var weatherContainer = document.querySelector("#current-weather-container");
-var searchedCityList = document.querySelector("#searched-city");
+let searchList = [];
+const searchSubmitBtn = document.querySelector("#search-submit");
+const cityNameInput = document.querySelector("#city-name");
+const weatherContainer = document.querySelector("#current-weather-container");
+const searchedCityList = document.querySelector("#searched-city");
 
-var fiveDayBrodcast = document.querySelector("#broadcast-container");
-var searchedCityBtns = document.querySelector("#searched-btn");
+const fiveDayBrodcast = document.querySelector("#broadcast-container");
+const searchedCityBtns = document.querySelector("#searched-btn");
+
+const formSumbit = function (e) {
+  e.preventDefault();
+  const cityName = cityNameInput.value.trim();
+  if (cityName) {
+    // getCityWeather(cityName);
+    // get5Day(cityName);
+    searchList.unshift({ cityName });
+    cityNameInput.value = "";
+  } else {
+    alert("Please enter a City");
+  }
+  saveSearchedCity();
+  //   pastSearch(cityName);
+  console.log(searchList);
+};
+
+const saveSearchedCity = function () {
+  localStorage.setItem("cities", JSON.stringify(searchList));
+};
+
+searchSubmitBtn.addEventListener("click", formSumbit);
