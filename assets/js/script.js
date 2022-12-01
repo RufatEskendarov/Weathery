70672e8474a5c4b4ce4d86469e6655dbeaa42de1
Eskendarov <Eskendarov.Rufat@sunbury.ca>
2022-12-01 00:01:49 -0400

    JS starter file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+"use strict";
 
+var searchList = [];
+var searchSubmitBtn = document.querySelector("#search-submit");
+var cityNameInput = document.querySelector("#city-name");
+var weatherContainer = document.querySelector("#current-weather-container");
+var searchedCityList = document.querySelector("#searched-city");
+
+var fiveDayBrodcast = document.querySelector("#broadcast-container");
+var searchedCityBtns = document.querySelector("#searched-btn");
