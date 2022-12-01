7de17f27d6855901b6a6d0d6963cf0591a7f47f4
Eskendarov <Eskendarov.Rufat@sunbury.ca>
2022-12-01 00:44:13 -0400

    Updeted function for rendering current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,69 @@
 "use strict";
 
 let searchList = [];
-const searchSubmitBtn = document.querySelector("#search-submit");
-const cityNameInput = document.querySelector("#city-name");
-const weatherContainer = document.querySelector("#current-weather-container");
-const searchedCityList = document.querySelector("#searched-city");
-
-const fiveDayBrodcast = document.querySelector("#broadcast-container");
-const searchedCityBtns = document.querySelector("#searched-btn");
+const searchSubmitBtn = document.getElementById("search-submit");
+const cityNameInput = document.getElementById("city-name");
+const currentWeatherContainer = document.getElementById(
+  "current-weather-container"
+);
+const searchedCityList = document.getElementById("searched-city");
+const weatherContainer = document.getElementById("current");
+const fiveDayBrodcast = document.getElementById("broadcast-container");
+const searchedCityBtns = document.getElementById("searched-btn");
 
 const formSumbit = function (e) {
   e.preventDefault();
-  const cityName = cityNameInput.value.trim();
+  let cityName = cityNameInput.value.trim();
+  console.log(cityNameInput.value);
   if (cityName) {
-    // getCityWeather(cityName);
+    getCityWeather(cityName);
     // get5Day(cityName);
     searchList.unshift({ cityName });
     cityNameInput.value = "";
   } else {
     alert("Please enter a City");
   }
-  saveSearchedCity();
+  //   saveSearchedCity();
   //   pastSearch(cityName);
   console.log(searchList);
 };
 
 const saveSearchedCity = function () {
   localStorage.setItem("cities", JSON.stringify(searchList));
+};
+
+const getCityWeather = function (city) {
+  const apiKey = "c41089c13b9d3a93f5373eaf06e846f3";
+  // var apiKey = "844421298d794574c100e3409cee0499";
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      displayWeather(data, city);
+    });
+  });
+};
+
+const displayWeather = function (weather, searchCity) {
+  //clear old content
+  const html = `
+    <h3 id="searched-city" class="text-warning">
+      ${searchCity} (${moment(weather.dt.value).format(
+    "MMM D, YYYY"
+  )}) <img src ="https://openweathermap.org/img/wn/${
+    weather.weather[0].icon
+  }@2x.png">
+    </h3>
+    <div id="current-weather-container" class="list-group">
+      <ul class="text-warning info">
+        <li>Temperature: ${weather.main.temp} °F</li>
+        <li>Humidity: ${weather.main.humidity}%</li>
+        <li>Wind speed: ${weather.wind.speed} MPH</li>
+      </ul>
+    </div>
+  `;
+
+  weatherContainer.insertAdjacentHTML("afterbegin", html);
 };
 
 searchSubmitBtn.addEventListener("click", formSumbit);
